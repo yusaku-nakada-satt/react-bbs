8010e528ef3b00af8b96c3edf0d91c4b9a374dcd
yusaku-nakada-satt <y_nakada@satt.jp>
2024-01-30 13:17:31 +0900

    titleを取ってくる関数を切り出した

--- react-bbs/src/App.tsx
+++ react-bbs/src/App.tsx
@@ -6,35 +6,35 @@ function App() {
   const [title, setTitle] = useState<Array<Map<string, string>>>([]);
   const url = "https://railway.bulletinboard.techtrain.dev/";
 
-  useEffect(() => {
-    const fetchTitles = async () => {
-      let titleList: Array<Map<string, string>> = [];
-      let whileFlg: boolean = true;
-      let i = 0;
-      while (whileFlg) {
-        try {
-          const response = await fetch(`${url}/threads?offset=${i}0`);
-          const data = await response.json();
-          const mapData = data.map(
-            (item: any) => new Map(Object.entries(item))
-          );
-          titleList = titleList.concat(mapData);
-          if (data.length < 10) {
-            whileFlg = false;
-            break;
-          }
-          i++;
-        } catch (error) {
-          console.error(error);
-          break;
+  const fetchTitles = async () => {
+    let titleList: Array<Map<string, string>> = [];
+    let hasMoreData: boolean = true;
+    let i = 0;
+    try {
+      while (hasMoreData) {
+        const response = await fetch(`${url}/threads?offset=${i}0`);
+        const data = await response.json();
+        const mapData = data.map((item: any) => new Map(Object.entries(item)));
+        titleList = [...titleList, ...mapData];
+        if (data.length < 10) {
+          hasMoreData = false;
         }
+        i++;
       }
       console.info(`掲示板取得件数:${titleList.length}`);
-      setTitle(titleList);
-    };
+      return titleList;
+    } catch (error) {
+      console.error(error);
+      return [];
+    }
+  };
 
-    fetchTitles();
+  useEffect(() => {
+    fetchTitles().then((result) => {
+      setTitle(result);
+    });
   }, []);
+
   return (
     <div className="App">
       <Header />
