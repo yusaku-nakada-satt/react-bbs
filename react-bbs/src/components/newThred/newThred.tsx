b17bad9f047a949dffce7adf25e1489f87646dae
yusaku-nakada-satt <y_nakada@satt.jp>
2024-02-09 09:57:14 +0900

    エラーハンドリング実装

--- react-bbs/src/components/newThred/newThred.tsx
+++ react-bbs/src/components/newThred/newThred.tsx
@@ -1,10 +1,11 @@
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, ReactNode, useState } from "react";
 import { Link } from "react-router-dom";
 import "./newThred.css";
 
 function NewThred() {
   const url = "https://railway.bulletinboard.techtrain.dev/";
   const [thredTitle, setThredTitle] = useState<string>("");
+  const [message, setMessage] = useState<any>();
 
   const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
     setThredTitle(event.target.value);
@@ -15,24 +16,33 @@ function NewThred() {
     const data = {
       title: thredTitle,
     };
-    const response = await fetch(`${url}/threads`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    });
-    if (response.status === 200) {
-      console.info("スレッドを作成しました");
-      return;
+
+    try {
+      const response = await fetch(`${url}/threads`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (response.ok) {
+        setMessage(<span className="message">スレッドを作成しました</span>);
+        console.log("スレッドを作成しました");
+      }
+    } catch (error) {
+      console.error("エラーが発生しました:", error);
+      setMessage(
+        <span className="error-message">スレッドの作成に失敗しました。</span>
+      );
     }
-    console.error("スレッド作成に失敗しました");
   };
 
   return (
     <div className="App-body">
       <h1>スレッド新規作成</h1>
       <div className="content">
+        <div className="message">{message}</div>
         <div className="input">
           <label htmlFor="title">スレッドタイトル</label>
           <input
